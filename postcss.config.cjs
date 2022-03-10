a2a475256854b78f14541273ad0fa73001fd5829
ryanfiller <ryanfiller89@gmail.com>
2022-03-10 09:55:51 -0600

    add dark mode and some theme colors

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -2,6 +2,7 @@ const autoprefixer = require('autoprefixer')
 const cssnano = require('cssnano')
 const postcssJitProps = require('postcss-jit-props')
 const OpenProps = require('open-props')
+const customMedia = require('postcss-custom-media')
 
 const mode = process.env.NODE_ENV
 const dev = mode === 'development'
@@ -10,12 +11,17 @@ const config = {
   plugins: [
     autoprefixer(),
     postcssJitProps(OpenProps),
+    customMedia({
+      importFrom: [
+        'open-props/media'
+      ]
+    }),
 
     !dev &&
       cssnano({
-        preset: 'default',
-      }),
-  ],
+        preset: 'default'
+      })
+  ]
 }
 
 module.exports = config
