821d821e456dd0504b9365a2bcff1c284f142327
ryanfiller <ryanfiller89@gmail.com>
2022-03-09 21:31:32 -0600

    add a loader

---
+++ src/lib/components/loader.svelte
@@ -0,0 +1,8 @@
+<style>
+  div {
+    animation: var(--animation-spin);
+    text-align: center;
+  }
+</style>
+
+<div>loading</div>
--- src/routes/rooms.svelte
+++ src/routes/rooms.svelte
@@ -1,19 +1,32 @@
-<h1>rooms</h1>
-
 <script>
   import supabase from '$lib/supabase'
+  import Loader from '$lib/components/loader.svelte'
 
   async function getData() {
-    let { data: boxes, error } = await supabase
+    let { data: rooms, error } = await supabase
       .from('rooms')
-      .select('*')
-    return boxes
-  }
-  
+      .select('name, description')
+    return rooms
+  }  
 </script>
 
-{#await getData() then data}
-<pre>
-{JSON.stringify(data, null, 2)}
-</pre>
-{/await}
+<main>
+  {#await getData()}
+    <Loader />
+  {:then rooms}
+    {#each rooms as room}
+      {#if room.description}
+        <details>
+          <summary>
+            <a href={`/${room.name}`}>{room.name}</a>
+          </summary>
+          {room.description}
+        </details>
+      {:else}
+        <summary>
+          <a href={`/${room.name}`}>{room.name}</a>
+        </summary>
+      {/if}
+    {/each}
+  {/await}
+</main>
