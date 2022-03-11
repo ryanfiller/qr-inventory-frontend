cb679171377fb1173b401980c737cdd378db9454
ryanfiller <ryanfiller89@gmail.com>
2022-03-11 09:25:32 -0600

    breadcrumbs component

---
+++ src/lib/components/breadcrumbs.svelte
@@ -0,0 +1,25 @@
+<script>
+  import breadcrumbs from '$lib/stores/breadcrumbs.js'
+</script>
+
+<style>
+  ol {
+    list-style: none;
+    padding: 0;
+    display: flex;
+  }
+
+  li::after {
+    content: '>';
+  }
+</style>
+
+<ol>
+  {#each $breadcrumbs as breadcrumb}
+    <li>
+      <a href={breadcrumb.link}>
+        {breadcrumb.text}
+      </a>
+    </li>
+  {/each}
+</ol>
