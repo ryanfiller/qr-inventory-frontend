7d0cbe32b4a1aac37eedb96a5f78c4cd20b3d6b0
ryanfiller <ryanfiller89@gmail.com>
2022-03-15 08:49:36 -0500

    do some layout on for the nav

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -4,17 +4,30 @@
 
   import Breadcrumbs from '$lib/components/breadcrumbs.svelte'
 
-  const segment = $page.url.pathname.split('/').filter(Boolean)[0]
+  $: segment = $page.url.pathname.split('/').filter(Boolean)[0] || 'homepage'
 </script>
 
 <style>
   :global(body) {
     padding: var(--size-3);
     display: grid;
+    align-items: center;
     grid-template-columns: 100%;
-    grid-template-rows: auto 1fr auto;
+    grid-template-rows: auto auto 1fr auto auto;
     gap: var(--size-3);
+    grid-template-areas: "lists" "header" "main" "actions" "footer";
   }
+
+  header { grid-area: header; }
+
+  main { grid-area: main; }
+
+  nav.lists { grid-area: lists; }
+
+  nav.actions { grid-area: actions; }
+
+  footer { grid-area: footer; }
+
 
   main {
     /* display: grid;
@@ -23,26 +36,32 @@
   }
 </style>
 
-<header>
-  <!-- TODO - scan, search, all... -->
-  <nav>
-    <ul>
-      <li>
-        <a href='/'>home</a>
-      </li>
-      <li>
-        <a href='/rooms'>all rooms</a>
-      </li>
-      <li>
-        <a href='/shelves'>all shelves</a>
-      </li>
-      <li>
-        <a href='/boxes'>all boxes</a>
-      </li>
-    </ul>
-  </nav>
-  
+<nav class='actions'>
+  <ul>
+    <li>
+      <a class='button' href='/'>scan</a>
+    </li>
+    <li>
+      <a class='button' href='/search'>search</a>
+    </li>
+  </ul>
+</nav>
 
+<nav class='lists'>
+  <ul>
+    <li>
+      <a class='button' href='/rooms'>all rooms</a>
+    </li>
+    <li>
+      <a class='button' href='/shelves'>all shelves</a>
+    </li>
+    <li>
+      <a class='button' href='/boxes'>all boxes</a>
+    </li>
+  </ul>
+</nav>
+
+<header>
   {#if $page.error}
     <h1>oh shit!</h1>
   {:else}
